a6722bc0729ae7fc236b0b07f50cd1ca89ca56cc
Flávia Reis <flaviarster@gmail.com>
2026-01-21 11:45:46 -0300

    feat: update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 print("Hello, World!")
+
+# Variável e input
+nome_usuario = input("Digite o seu nome: ")
+
+# Output
+print(nome_usuario)
+
+# Tipos de dados
+## String
+str = "Sou uma string"
+## Number
+numero = 30
+## Boolean
+booleano = False
+## None
+nulo = None
+
+print(nulo)
+
+#Condicional
+if(booleano):
+    print("Verdade!")
+else:
+    print("Mentira!")
